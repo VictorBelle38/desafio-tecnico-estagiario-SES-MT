71897132a84dcd1af3cf27c1e0c11a441ea1bed2
VictorBelle38 <victorbelle38@gmail.com>
2025-04-29 15:38:29 -0400

    Atualizar Task model: Adicionado campos 'fillable' para título, descrição e status.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,11 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'status'];
+
     public function user()
-{
-    return $this->belongsTo(User::class);
-}
+    {
+        return $this->belongsTo(User::class);
+    }
 }
